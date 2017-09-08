97fd5b4dfff2bf4748929306b1b9d1dee281914d
Iago Mosqueira <iago@mosqueira.eu>
2017-09-08 11:28:40 +0200

    FLBRP plot ready

--- R/plots.R
+++ R/plots.R
@@ -12,37 +12,70 @@ setMethod("plot", signature("FLBRP", "missing"),
 
     # EXTRACT metrics
     df <- model.frame(metrics(x,
-      list(ssb=ssb, f=fbar, rec=rec, catch=catch, profit=profit)), drop=TRUE)
+      list(ssb=ssb, harvest=fbar, rec=rec, yield=landings, profit=profit)), drop=TRUE)
 
     # refpts
     rps <- refpts(x)
 
     # SUBSET df
-    df <- subset(df, f <= c(rps['crash', 'harvest']))
+    df <- subset(df, harvest <= c(rps['crash', 'harvest']))
 
     # NO economics
     panels <- list(
-      P1=c(x="f", y="ssb", panel="Equilibrium SSB v. F"),
+      P1=c(x="harvest", y="ssb", panel="Equilibrium SSB v. F"),
       P2=c(x="ssb", y="rec", panel="Equilibrium Recruitment v. SSB"),
-      P3=c(x="f", y="catch", panel="Equilibrium Yield v. F"),
-      P4=c(x="ssb", y="catch", panel="Equilibrium Yield v. SSB"))
+      P3=c(x="harvest", y="yield", panel="Equilibrium Yield v. F"),
+      P4=c(x="ssb", y="yield", panel="Equilibrium Yield v. SSB"))
 
     # WITH economics
-    panels <- c(panels, list(
-      P5=c(x="f", y="profit", panel="Equilibrium Profit v. F"),
-      P6=c(x="ssb", y="profit", panel="Equilibrium Profit v. SSB")))
+    if(!all(is.na(rps[, 'profit']))) {
+      panels <- c(panels, list(
+        P5=c(x="harvest", y="profit", panel="Equilibrium Profit v. F"),
+        P6=c(x="ssb", y="profit", panel="Equilibrium Profit v. SSB")))
+    }
                              
     # APPLY over panels to extract x, y and panel for each element
-    dat <- lapply(panels, function(x) {
-      data.frame(x=df[,x['x']], y=df[,x['y']], panel=x['panel'], row.names=NULL)
+    dat <- lapply(panels, function(p) {
+      data.frame(x=df[,p['x']], y=df[,p['y']], panel=p['panel'], row.names=NULL)
     })
 
     # RBIND into single df
     dat <- do.call(rbind, c(dat, list(make.row.names = FALSE)))
 
     # PLOT
+    p <- ggplot(dat, aes(x=x, y=y)) + geom_line() +
+      facet_wrap(~panel, scales="free", ncol=2) +
+      xlab("") + ylab("")
 
-    ggplot(dat, aes(x=x, y=y)) + geom_line() +
-      facet_wrap(~panel, scales="free", ncol=2)
+    # PLOT refpts
+    if(refpts) {
+      rpdat <- lapply(panels, function(p) {
+        # CBIND x, + refpt, iter ...
+        cbind(as(rps[, p['x']], 'data.frame')[, -2],
+        # ... y, panel
+        y=c(rps[,p['y']]), panel=unname(p['panel']))
+      })
+      rpdat <- do.call(rbind, c(rpdat, list(make.row.names = FALSE)))
+
+      p <- p + geom_point(data=rpdat, aes(x=data, y=y, colour=refpt))
+    }
+
+    # PLOT observations
+    if(obs) {
+    
+      dfo <- model.frame(metrics(x,
+        list(ssb=ssb.obs, harvest=fbar.obs, rec=rec.obs, yield=landings.obs)),
+        drop=TRUE)
+    
+      # APPLY over panels to extract x, y and panel for each element
+      dato <- lapply(panels[1:4], function(p)
+        data.frame(x=dfo[,p['x']], y=dfo[,p['y']], panel=p['panel'], row.names=NULL))
+      
+      dato <- do.call(rbind, c(dato, list(make.row.names = FALSE)))
+
+      p <- p + geom_point(data=dato)
+    }
+
+    return(p)
   }
 )
